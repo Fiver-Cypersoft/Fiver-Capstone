4718e73d39bb4ba4235eab2041a9791ff4ec755b
Hoàng Các Loan Châu <hoangchau741@gmail.com>
2023-11-20 02:08:02 +0700

    fix layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import logo from "./logo.svg";
 import "./App.css";
 import Spinner from "./component/Spinner/Spinner";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./template/Layout";
+import Profile from "./page/Profile/Profile";
+import Layout from "./page/Template/Layout";
 
 function App() {
   return (
     <div>
       <Spinner />
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route
+            path="/profile"
+            element={
+              <Layout>
+                <Profile />
+              </Layout>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </div>
   );
